b5bfc05659ce72ca520069a0f4bc3da6f9ff8a19
Kazmi <apple@Apples-iMac-2.local>
2019-08-07 10:04:35 +0500

    first commit

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,14 @@
+variable "sgec2" {}
+
+variable "subnetId" {}
+
+variable "namePrefix" {}
+
+variable "instanceAmi" {
+    default = "ami-056ee704806822732"
+}
+
+variable "instanceType" {
+    default = "t2.micro"
+}
+
